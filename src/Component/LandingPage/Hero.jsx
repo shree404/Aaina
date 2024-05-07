e7bcbf38c73035643141067a468dcab4d885af67
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-07 12:48:34 +0545

    Hero Section Added

--- src/Component/LandingPage/Hero.jsx
+++ src/Component/LandingPage/Hero.jsx
@@ -1,6 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import img from '../../Assets/LandingPage/hero.png';
+
 function Hero(){
     return(
         <>
+        <Container fluid>
+            
+            <Row>
+                <Col md={12} sm={12} xs={12}>
+                <img src={img} alt="Hero Image"  className='Hero-image'/>
+                </Col>
+            </Row>
+        </Container>
         </>
     )
 }
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import Nav from "../Component/LandingPage/Nav";
+import Hero from "../Component/LandingPage/Hero";
 
 function LandingPage(){
     return(
         <>
         <Nav/>
+        <Hero/>
         
         </>
     )
