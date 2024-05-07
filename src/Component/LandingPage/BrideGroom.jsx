901fd55f54d62903f9fda6ee1c48d33ed2632364
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-07 15:30:46 +0545

    Kurti and Bride Groom Section added

---
+++ src/Component/LandingPage/BrideGroom.jsx
@@ -0,0 +1,126 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import bg1 from '../../Assets/LandingPage/bd1.png';
+import bg2 from '../../Assets/LandingPage/bd2.png';
+import img1 from '../../Assets/LandingPage/bdimg1.png';
+import img2 from '../../Assets/LandingPage/bdimg2.png';
+import img3 from '../../Assets/LandingPage/bdimg3.png';
+import img4 from '../../Assets/LandingPage/bdimg4.png';
+
+function BrideGroom(){
+    let imageArray=[
+        {
+        Image: img1,
+        Name: 'Bride Saree',
+        price: 'Rs.28000'
+        },
+        {
+        Image: img2,
+        Name: 'Groom wedding set',
+        price: 'Rs.28000'
+        },
+        {
+        Image: img3,
+        Name: 'Bride Saree',
+        price: 'Rs.28000'
+        },
+        {
+        Image: img4,
+        Name: 'Bride Saree',
+        price: 'Rs.28000'
+        }
+    ]
+    return(
+        <>
+          <div className="NewProduct">
+        <Container>
+          <div>
+            <Row className="justify-content-md-sm-xs-center">
+              <Col md={12} sm={12} xs={12} >
+                <h1>kurti</h1>
+              </Col>
+            </Row>
+          </div>
+          <div>
+            <Row>
+              <Col md={4} sm={12} xs={12}>
+                <div className="container-kurtiSection-buynow--kurti1">
+                  <img src={bg2} alt="Image of Kurta" className="kurta1" />
+                </div>
+              </Col>
+              <Col md={4} sm={12} xs={12}>
+              <p className="container-BrideandGroomSection-buynow-middleSection__description"><span className="container-BrideandGroomSection-buynow-middleSection__description__word">Handcrafted</span>  Masterpieces <br /> for Your Special Day </p>
+                <p className="container-BrideandGroomSection-buynow-middleSection__description2">Explore Our Bridal & Groom <br /> Collection</p>
+                <button className="container-BrideandGroomSection-buynow-middleSection__button">BUY NOW</button>
+              </Col>
+              <Col md={4} sm={12} xs={12}>
+                <div className="container-kurtiSection-buynow--kurti2">
+                  <img
+                    src={bg1}
+                    alt="Image of girl wearing kurta"
+                    className="kurta2"
+                  />
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="NewProduct-md">
+            <Row className="justify-content-md-sm-xs-center">
+              {imageArray.map((product, index) => (
+                <Col md={3} sm={6} xs={12}>
+                  <div
+                    key={index}
+                    className="product-details-container-single-product"
+                  >
+                    <article>
+                      <img
+                        src={product.Image}
+                        alt="Image of Maroon Lehenga"
+                      />
+                      <p className="product-details-container-single-product__name">
+                        {product.Name}{" "}
+                      </p>
+                      <p className="product-details-container-single-product__price">
+                        {" "}
+                        {product.price}{" "}
+                      </p>
+                    </article>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+          <div className="NewProduct-S-md">
+            <Row>
+              {imageArray.map((product, index) => (
+                <Col>
+                  <div
+                    key={index}
+                    className="product-details-container-single-product"
+                  >
+                    <article>
+                      <img
+                        src={product.Image}
+                        alt="Image of Maroon Lehenga"
+                      />
+                      <p className="product-details-container-single-product__name">
+                        {product.Name}{" "}
+                      </p>
+                      <p className="product-details-container-single-product__price">
+                        {" "}
+                        {product.price}{" "}
+                      </p>
+                    </article>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
+      </div>
+        </>
+    )
+}
+
+export default BrideGroom;
---
+++ src/Component/LandingPage/Kurti.jsx
@@ -0,0 +1,137 @@
+import img1 from "../../Assets/LandingPage/kurti1.png";
+import img2 from "../../Assets/LandingPage/kutri2.png";
+import kurtha1 from "../../Assets/LandingPage/kurta1.png";
+import kurtha2 from "../../Assets/LandingPage/kurta2.png";
+import kurtha3 from "../../Assets/LandingPage/kurta3.png";
+import kurtha4 from "../../Assets/LandingPage/kurta4.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Kurti() {
+  let kurtiarray = [
+    {
+      kurtiImage: kurtha1,
+      kurtiName: "Bride Saree",
+      kurtiprice: "Rs.28000",
+    },
+    {
+      kurtiImage: kurtha2,
+      kurtiName: "Bride Saree",
+      kurtiprice: "Rs.28000",
+    },
+    {
+      kurtiImage: kurtha3,
+      kurtiName: "Bride Saree",
+      kurtiprice: "Rs.28000",
+    },
+    {
+      kurtiImage: kurtha4,
+      kurtiName: "Bride Saree",
+      kurtiprice: "Rs.28000",
+    },
+  ];
+  return (
+    <>
+      <div className="NewProduct">
+        <Container>
+          <div>
+            <Row className="justify-content-md-sm-xs-center">
+              <Col md={12} sm={12} xs={12} >
+                <h1>kurti</h1>
+              </Col>
+            </Row>
+          </div>
+          <div>
+            <Row>
+              <Col md={4} sm={12} xs={12}>
+                <div className="container-kurtiSection-buynow--kurti1">
+                  <img src={img2} alt="Image of Kurta" className="kurta1" />
+                </div>
+              </Col>
+              <Col md={4} sm={12} xs={12}>
+                <p className="container-kurtiSection-buynow-middleSection__description">
+                  Explore Our{" "}
+                  <span className="container-kurtiSection-buynow-middleSection__description__kurti-word">
+                    {" "}
+                    Kurti <br />{" "}
+                  </span>{" "}
+                  Collection
+                </p>
+                <p className="container-kurtiSection-buynow-middleSection__description2">
+                  Explore Our Bridal & Groom <br /> Collection
+                </p>
+                <button className="container-kurtiSection-buynow-middleSection__button">
+                  BUY NOW
+                </button>
+              </Col>
+              <Col md={4} sm={12} xs={12}>
+                <div className="container-kurtiSection-buynow--kurti2">
+                  <img
+                    src={img1}
+                    alt="Image of girl wearing kurta"
+                    className="kurta2"
+                  />
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="NewProduct-md">
+            <Row className="justify-content-md-sm-xs-center">
+              {kurtiarray.map((product, index) => (
+                <Col md={3} sm={6} xs={12}>
+                  <div
+                    key={index}
+                    className="product-details-container-single-product"
+                  >
+                    <article>
+                      <img
+                        src={product.kurtiImage}
+                        alt="Image of Maroon Lehenga"
+                      />
+                      <p className="product-details-container-single-product__name">
+                        {product.kurtiName}{" "}
+                      </p>
+                      <p className="product-details-container-single-product__price">
+                        {" "}
+                        {product.kurtiprice}{" "}
+                      </p>
+                    </article>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+          <div className="NewProduct-S-md">
+            <Row>
+              {kurtiarray.map((product, index) => (
+                <Col>
+                  <div
+                    key={index}
+                    className="product-details-container-single-product"
+                  >
+                    <article>
+                      <img
+                        src={product.kurtiImage}
+                        alt="Image of Maroon Lehenga"
+                      />
+                      <p className="product-details-container-single-product__name">
+                        {product.kurtiName}{" "}
+                      </p>
+                      <p className="product-details-container-single-product__price">
+                        {" "}
+                        {product.kurtiprice}{" "}
+                      </p>
+                    </article>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Kurti;
