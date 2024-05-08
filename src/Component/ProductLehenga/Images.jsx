1a7537aca31934e9b9a3403388842c7056feec62
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-08 14:29:30 +0545

    Responsive to all device

--- src/Component/ProductLehenga/Images.jsx
+++ src/Component/ProductLehenga/Images.jsx
@@ -4,37 +4,44 @@ import Col from "react-bootstrap/Col";
 import img1 from "../../Assets/ProductLehenga/1.png";
 import img2 from "../../Assets/ProductLehenga/2.png";
 import info from "../../Assets/ProductLehenga/info.svg";
-import {useState} from "react";
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import add from "../../Assets/ProductLehenga/add.svg";
 import remove from "../../Assets/ProductLehenga/remove.svg";
 import whatsapp from "../../Assets/ProductLehenga/whatsapp.svg";
 import up from "../../Assets/ProductLehenga/4.svg";
 import down from "../../Assets/ProductLehenga/3.svg";
 
-
 function ProductImages() {
-    const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
-    const decreaseValue = () => {
-      if (quantity > 1) {
-        setQuantity(quantity - 1);
-      }
-    };
-  
-    const increaseValue = () => {
-      setQuantity(quantity + 1);
-    };
+  const decreaseValue = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const increaseValue = () => {
+    setQuantity(quantity + 1);
+  };
   return (
     <>
       <div className="LehengaImages">
         <Container>
           <Row className="justify-content-sm-xs-center">
-            <Col md={{span:2, order:1}} sm={{span:12, order:2}} xs={{span:12, order:2}}>
+            <Col
+              md={{ span: 2, order: 1 }}
+              sm={{ span: 12, order: 2 }}
+              xs={{ span: 12, order: 2 }}
+            >
               <div className="LehengaImages-col1">
                 <div>
-                    <img src={up} alt="Up arrow" className="LehengaImages__uparrow" />
+                  <img
+                    src={up}
+                    alt="Up arrow"
+                    className="LehengaImages__uparrow"
+                  />
                 </div>
                 <div>
                   <img
@@ -65,46 +72,274 @@ function ProductImages() {
                   />
                 </div>
                 <div>
-                    <img src={down} alt="Down Arrow"  className="LehengaImages__downarrow"/>
+                  <img
+                    src={down}
+                    alt="Down Arrow"
+                    className="LehengaImages__downarrow"
+                  />
                 </div>
               </div>
             </Col>
-            <Col md={{span:5, order:2}} sm={{span:12, order:1}} xs={{span:12, order:1}} >
-              <img src={img2} alt="Image of Lehenga" />
+            <Col
+              md={{ span: 5, order: 2 }}
+              sm={{ span: 12, order: 1 }}
+              xs={{ span: 12, order: 1 }}
+            >
+              <img src={img2} alt="Image of Lehenga" className="LehengaImages__imgbig" />
             </Col>
-            <Col md={{span:5, order:3}} sm={{span:12, order:3}} xs={{span:12, order:3}} >
+            <Col
+              md={{ span: 5, order: 3 }}
+              sm={{ span: 12, order: 3 }}
+              xs={{ span: 12, order: 3 }}
+            >
               <p className="LehengaImages-description">
-                <span className=" LehengaImages-description__p1">Purple Embroidered Lehenga Choli</span> <br /> <br />
-                <span className="LehengaImages-description__p2">Rs. 6000</span> <br />
-                <span  className="LehengaImages-description__p3">
+                <span className=" LehengaImages-description__p1">
+                  Purple Embroidered Lehenga Choli
+                </span>{" "}
+                <br /> <br />
+                <span className="LehengaImages-description__p2">
+                  Rs. 6000
+                </span>{" "}
+                <br />
+                <span className="LehengaImages-description__p3">
                   Incl. of all tax
                   <img src={info} alt="Icon" />
-                </span> <br /> <br />
-                <span className="LehengaImages-description__p4">In Stock</span> <br /><br />
-                <span className="LehengaImages-description__p5">Select size</span> <br />
+                </span>{" "}
+                <br /> <br />
+                <span className="LehengaImages-description__p4">
+                  In Stock
+                </span>{" "}
+                <br />
+                <br />
+                <span className="LehengaImages-description__p5">
+                  Select size
+                </span>{" "}
+                <br />
                 <span>
                   {" "}
-                  <button className="LehengaImages-description__button">S</button> <button className="LehengaImages-description__button">M</button> <button className="LehengaImages-description__button">L</button>{" "}
-                  <button className="LehengaImages-description__button">XL</button>
+                  <button className="LehengaImages-description__button">
+                    S
+                  </button>{" "}
+                  <button className="LehengaImages-description__button">
+                    M
+                  </button>{" "}
+                  <button className="LehengaImages-description__button">
+                    L
+                  </button>{" "}
+                  <button className="LehengaImages-description__button">
+                    XL
+                  </button>
                 </span>
                 <br /> <br />
-                <span className="LehengaImages-description__p6">Quantity</span> <br />
+                <span className="LehengaImages-description__p6">
+                  Quantity
+                </span>{" "}
+                <br />
                 <span>
                   {" "}
                   <ButtonGroup aria-label="Basic example">
-      <Button variant="secondary" id="LehengaImages-description__button2"  onClick={decreaseValue}><img src={remove} alt="Remove icon" /></Button>
-      <Button variant="secondary" id="LehengaImages-description__button2">{quantity}</Button>
-      <Button variant="secondary" id="LehengaImages-description__button2" onClick={increaseValue}><img src={add} alt="Add icon" /></Button>
-    </ButtonGroup>
-                </span> <br /> <br />
+                    <Button
+                      variant="secondary"
+                      id="LehengaImages-description__button2"
+                      onClick={decreaseValue}
+                    >
+                      <img src={remove} alt="Remove icon" />
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      id="LehengaImages-description__button2"
+                    >
+                      {quantity}
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      id="LehengaImages-description__button2"
+                      onClick={increaseValue}
+                    >
+                      <img src={add} alt="Add icon" />
+                    </Button>
+                  </ButtonGroup>
+                </span>{" "}
+                <br /> <br />
                 <span>
                   {" "}
-                  <button className="LehengaImages-description__buttonadd"> Add to Bag</button> <button className="LehengaImages-description__buttonbuy"> <img src={whatsapp}alt="Icon of Whatsapp" /> Buy Now</button>
+                  <button className="LehengaImages-description__buttonadd">
+                    {" "}
+                    Add to Bag
+                  </button>{" "}
+                  <button className="LehengaImages-description__buttonbuy">
+                    {" "}
+                    <img src={whatsapp} alt="Icon of Whatsapp" /> Buy Now
+                  </button>
                 </span>
               </p>
             </Col>
           </Row>
+          <Row>
+            <Col>
+            <button className="LehengaImages-Row2__button">Description</button>
+            <button className="LehengaImages-Row2__button">INFORMATION</button>
+            </Col>
+          </Row>
         </Container>
+      </div>
+      <div className="LehengaImages-md">
+      <Container>
+        <Row>
+        <Col
+              md={{ span: 4, order: 1 }}
+              sm={{ span: 12, order: 2 }}
+              xs={{ span: 12, order: 2 }}
+            >
+              <div className="LehengaImages-col1">
+                <div>
+                  <img
+                    src={up}
+                    alt="Up arrow"
+                    className="LehengaImages__uparrow"
+                  />
+                </div>
+                <div>
+                  <img
+                    className="LehengaImages__img"
+                    src={img1}
+                    alt="Image of Lehenga"
+                  />{" "}
+                </div>
+                <div>
+                  <img
+                    className="LehengaImages__img"
+                    src={img1}
+                    alt="Image of Lehenga"
+                  />{" "}
+                </div>
+                <div>
+                  <img
+                    className="LehengaImages__img"
+                    src={img1}
+                    alt="Image of Lehenga"
+                  />{" "}
+                </div>
+                <div>
+                  <img
+                    className="LehengaImages__img"
+                    src={img1}
+                    alt="Image of Lehenga"
+                  />
+                </div>
+                <div>
+                  <img
+                    src={down}
+                    alt="Down Arrow"
+                    className="LehengaImages__downarrow"
+                  />
+                </div>
+              </div>
+            </Col>
+            <Col
+              md={{ span: 8, order: 2 }}
+              sm={{ span: 12, order: 1 }}
+              xs={{ span: 12, order: 1 }}
+            >
+              <img src={img2} alt="Image of Lehenga" className="LehengaImages__imgbig" />
+            </Col>
+        </Row>
+        <Row>
+        <Col
+              md={{ span: 12 }}
+              sm={{ span: 12 }}
+              xs={{ span: 12}}
+            >
+              <p className="LehengaImages-description">
+                <span className=" LehengaImages-description__p1">
+                  Purple Embroidered Lehenga Choli
+                </span>{" "}
+                <br /> <br />
+                <span className="LehengaImages-description__p2">
+                  Rs. 6000
+                </span>{" "}
+                <br />
+                <span className="LehengaImages-description__p3">
+                  Incl. of all tax
+                  <img src={info} alt="Icon" />
+                </span>{" "}
+                <br /> <br />
+                <span className="LehengaImages-description__p4">
+                  In Stock
+                </span>{" "}
+                <br />
+                <br />
+                <span className="LehengaImages-description__p5">
+                  Select size
+                </span>{" "}
+                <br />
+                <span>
+                  {" "}
+                  <button className="LehengaImages-description__button">
+                    S
+                  </button>{" "}
+                  <button className="LehengaImages-description__button">
+                    M
+                  </button>{" "}
+                  <button className="LehengaImages-description__button">
+                    L
+                  </button>{" "}
+                  <button className="LehengaImages-description__button">
+                    XL
+                  </button>
+                </span>
+                <br /> <br />
+                <span className="LehengaImages-description__p6">
+                  Quantity
+                </span>{" "}
+                <br />
+                <span>
+                  {" "}
+                  <ButtonGroup aria-label="Basic example">
+                    <Button
+                      variant="secondary"
+                      id="LehengaImages-description__button2"
+                      onClick={decreaseValue}
+                    >
+                      <img src={remove} alt="Remove icon" />
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      id="LehengaImages-description__button2"
+                    >
+                      {quantity}
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      id="LehengaImages-description__button2"
+                      onClick={increaseValue}
+                    >
+                      <img src={add} alt="Add icon" />
+                    </Button>
+                  </ButtonGroup>
+                </span>{" "}
+                <br /> <br />
+                <span>
+                  {" "}
+                  <button className="LehengaImages-description__buttonadd">
+                    {" "}
+                    Add to Bag
+                  </button>{" "}
+                  <button className="LehengaImages-description__buttonbuy">
+                    {" "}
+                    <img src={whatsapp} alt="Icon of Whatsapp" /> Buy Now
+                  </button>
+                </span>
+              </p>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            <button className="LehengaImages-Row2__button">Description</button>
+            <button className="LehengaImages-Row2__button">INFORMATION</button>
+            </Col>
+          </Row>
+      </Container>
       </div>
     </>
   );
