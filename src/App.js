4c04e457554c5a754cb12e17ac569674812ed201
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-21 10:36:32 +0545

    Page Not Found Added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import {
-  BrowserRouter,
-  Route,Routes,
-  Link
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
 } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import ProductLehenga from "./Pages/ProductLehenga";
 import Women from "./Pages/Women";
 import WomenLehenga from "./Pages/WomenLehenga";
 import WomenKurta from "./Pages/WomenKurta";
+import PageNotFound from "./Pages/PageNotFound";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<LandingPage/>} />,
+      <Route path="/home" element={<LandingPage/>} />,
+      <Route path="/product-lehenga" element={<ProductLehenga/>} />,
+      <Route path="/women" element={<Women/>} />,
+      <Route path="/women-lehenga" element={<WomenLehenga/>} />,
+      <Route path="/women-kurta" element={<WomenKurta/>} />,
+      <Route path="*" element={<PageNotFound />} />
+    </Route>
+  )
+);
 
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<LandingPage/>} />
-      <Route path="/home" element={<LandingPage/>} />
-      <Route path="/product-lehenga" element={<ProductLehenga/>} />
-      <Route path="/women" element={<Women/>} />
-      <Route path="/women-lehenga" element={<WomenLehenga/>} />
-      <Route path="/women-kurta" element={<WomenKurta/>} />
-      </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
