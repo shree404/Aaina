cbcc1b8217918920798d3c1510fa7ef4b3e7b2bb
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-22 11:16:00 +0545

    Route added

--- src/Component/LandingPage/BrideGroom.jsx
+++ src/Component/LandingPage/BrideGroom.jsx
@@ -1,43 +1,44 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import bg1 from '../../Assets/LandingPage/bd1.png';
-import bg2 from '../../Assets/LandingPage/bd2.png';
-import img1 from '../../Assets/LandingPage/bdimg1.png';
-import img2 from '../../Assets/LandingPage/bdimg2.png';
-import img3 from '../../Assets/LandingPage/bdimg3.png';
-import img4 from '../../Assets/LandingPage/bdimg4.png';
+import bg1 from "../../Assets/LandingPage/bd1.png";
+import bg2 from "../../Assets/LandingPage/bd2.png";
+import img1 from "../../Assets/LandingPage/bdimg1.png";
+import img2 from "../../Assets/LandingPage/bdimg2.png";
+import img3 from "../../Assets/LandingPage/bdimg3.png";
+import img4 from "../../Assets/LandingPage/bdimg4.png";
+import { Link } from "react-router-dom";
 
-function BrideGroom(){
-    let imageArray=[
-        {
-        Image: img1,
-        Name: 'Bride Saree',
-        price: 'Rs.28000'
-        },
-        {
-        Image: img2,
-        Name: 'Groom wedding set',
-        price: 'Rs.28000'
-        },
-        {
-        Image: img3,
-        Name: 'Bride Saree',
-        price: 'Rs.28000'
-        },
-        {
-        Image: img4,
-        Name: 'Bride Saree',
-        price: 'Rs.28000'
-        }
-    ]
-    return(
-        <>
-          <div className="NewProduct">
+function BrideGroom() {
+  let imageArray = [
+    {
+      Image: img1,
+      Name: "Bride Saree",
+      price: "Rs.28000",
+    },
+    {
+      Image: img2,
+      Name: "Groom wedding set",
+      price: "Rs.28000",
+    },
+    {
+      Image: img3,
+      Name: "Bride Saree",
+      price: "Rs.28000",
+    },
+    {
+      Image: img4,
+      Name: "Bride Saree",
+      price: "Rs.28000",
+    },
+  ];
+  return (
+    <>
+      <div className="NewProduct">
         <Container>
           <div>
             <Row className="justify-content-md-sm-xs-center">
-              <Col md={12} sm={12} xs={12} >
+              <Col md={12} sm={12} xs={12}>
                 <h1>Bride and Groom</h1>
               </Col>
             </Row>
@@ -50,9 +51,18 @@ function BrideGroom(){
                 </div>
               </Col>
               <Col md={4} sm={12} xs={12}>
-              <p className="container-BrideandGroomSection-buynow-middleSection__description"><span className="container-BrideandGroomSection-buynow-middleSection__description__word">Handcrafted</span>  Masterpieces <br /> for Your Special Day </p>
-                <p className="container-BrideandGroomSection-buynow-middleSection__description2">Explore Our Bridal & Groom <br /> Collection</p>
-                <button className="container-BrideandGroomSection-buynow-middleSection__button">BUY NOW</button>
+                <p className="container-BrideandGroomSection-buynow-middleSection__description">
+                  <span className="container-BrideandGroomSection-buynow-middleSection__description__word">
+                    Handcrafted
+                  </span>{" "}
+                  Masterpieces <br /> for Your Special Day{" "}
+                </p>
+                <p className="container-BrideandGroomSection-buynow-middleSection__description2">
+                  Explore Our Bridal & Groom <br /> Collection
+                </p>
+                <button className="container-BrideandGroomSection-buynow-middleSection__button">
+                  BUY NOW
+                </button>
               </Col>
               <Col md={4} sm={12} xs={12}>
                 <div className="container-kurtiSection-buynow--kurti2">
@@ -74,17 +84,22 @@ function BrideGroom(){
                     className="product-details-container-single-product"
                   >
                     <article>
-                      <img
-                        src={product.Image}
-                        alt="Image of Maroon Lehenga"
-                      />
-                      <p className="product-details-container-single-product__name">
-                        {product.Name}{" "}
-                      </p>
-                      <p className="product-details-container-single-product__price">
-                        {" "}
-                        {product.price}{" "}
-                      </p>
+                      <Link
+                        to="/product-lehenga"
+                        className="product-details-container-single-product"
+                      >
+                        <img
+                          src={product.Image}
+                          alt="Image of Maroon Lehenga"
+                        />
+                        <p className="product-details-container-single-product__name">
+                          {product.Name}{" "}
+                        </p>
+                        <p className="product-details-container-single-product__price">
+                          {" "}
+                          {product.price}{" "}
+                        </p>
+                      </Link>
                     </article>
                   </div>
                 </Col>
@@ -100,17 +115,22 @@ function BrideGroom(){
                     className="product-details-container-single-product"
                   >
                     <article>
-                      <img
-                        src={product.Image}
-                        alt="Image of Maroon Lehenga"
-                      />
-                      <p className="product-details-container-single-product__name">
-                        {product.Name}{" "}
-                      </p>
-                      <p className="product-details-container-single-product__price">
-                        {" "}
-                        {product.price}{" "}
-                      </p>
+                      <Link
+                        to="/product-lehenga"
+                        className="product-details-container-single-product"
+                      >
+                        <img
+                          src={product.Image}
+                          alt="Image of Maroon Lehenga"
+                        />
+                        <p className="product-details-container-single-product__name">
+                          {product.Name}{" "}
+                        </p>
+                        <p className="product-details-container-single-product__price">
+                          {" "}
+                          {product.price}{" "}
+                        </p>
+                      </Link>
                     </article>
                   </div>
                 </Col>
@@ -119,8 +139,8 @@ function BrideGroom(){
           </div>
         </Container>
       </div>
-        </>
-    )
+    </>
+  );
 }
 
 export default BrideGroom;
--- src/Component/LandingPage/Newproduct.jsx
+++ src/Component/LandingPage/Newproduct.jsx
@@ -1,79 +1,112 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import product1 from '../../Assets/LandingPage/newproduct1.png';
-import product2 from '../../Assets/LandingPage/newproduct2.png';
-import product3 from '../../Assets/LandingPage/newproduct3.png';
-import product4 from '../../Assets/LandingPage/newproduct4.png';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import product1 from "../../Assets/LandingPage/newproduct1.png";
+import product2 from "../../Assets/LandingPage/newproduct2.png";
+import product3 from "../../Assets/LandingPage/newproduct3.png";
+import product4 from "../../Assets/LandingPage/newproduct4.png";
+import { Link } from "react-router-dom"; 
 
-function NewProduct(){
-    let newproductarray = [
-        {
-            productimgae:product1,
-            productName:"Lehenga Blouse Set ",
-            productPrice:"Rs.6000"
-        },
-        {
-            productimgae:product2,
-            productName:"Lehenga Blouse Set ",
-            productPrice:"Rs.6000"
-        },
-        {
-            productimgae:product3,
-            productName:"Kurtha ",
-            productPrice:"Rs.6000"
-        },
-        {
-            productimgae:product4,
-            productName:"Lehenga Blouse Set ",
-            productPrice:"Rs.6000"
-        }
-    ]
-    return(
-        <>
-        <div className='NewProduct'>
-            <Container>
-                <div>
-                    <Row className='justify-content-md-sm-xs-center'>
-                        <Col md={12} sm={12} xs={12}>
-                        <h1>New Products</h1>
-                        </Col>
-                    </Row>
-                </div>
-                <div className='NewProduct-md'>
-                    <Row className='justify-content-md-sm-xs-center'>
-                    {newproductarray.map((product,index) => (
-                        <Col md={3} sm={6} xs={12}>
-                       <div key={index} className="product-details-container-single-product">
-                       <article>
-                       <img src={product.productimgae} alt="Image of Maroon Lehenga" />
-                       <p className="product-details-container-single-product__name">{product.productName} </p>
-                       <p className="product-details-container-single-product__price"> {product.productPrice} </p>
-                   </article>
-                       </div>  
-                       </Col>
-                    ))}
-                    </Row>
-                    </div>
-                    <div className='NewProduct-S-md'>
-                        <Row>
-                        {newproductarray.map((product,index) => (
-                        <Col >
-                       <div key={index} className="product-details-container-single-product">
-                       <article>
-                       <img src={product.productimgae} alt="Image of Maroon Lehenga" />
-                       <p className="product-details-container-single-product__name">{product.productName} </p>
-                       <p className="product-details-container-single-product__price"> {product.productPrice} </p>
-                   </article>
-                       </div>  
-                       </Col>
-                    ))}
-                        </Row>
-                    </div>
-            </Container>
-        </div>
-        </>
-    )
+function NewProduct() {
+  let newproductarray = [
+    {
+      productimgae: product1,
+      productName: "Lehenga Blouse Set ",
+      productPrice: "Rs.6000",
+    },
+    {
+      productimgae: product2,
+      productName: "Lehenga Blouse Set ",
+      productPrice: "Rs.6000",
+    },
+    {
+      productimgae: product3,
+      productName: "Kurtha ",
+      productPrice: "Rs.6000",
+    },
+    {
+      productimgae: product4,
+      productName: "Lehenga Blouse Set ",
+      productPrice: "Rs.6000",
+    },
+  ];
+  return (
+    <>
+      <div className="NewProduct">
+        <Container>
+          <div>
+            <Row className="justify-content-md-sm-xs-center">
+              <Col md={12} sm={12} xs={12}>
+                <h1>New Products</h1>
+              </Col>
+            </Row>
+          </div>
+          <div className="NewProduct-md">
+            <Row className="justify-content-md-sm-xs-center">
+              {newproductarray.map((product, index) => (
+                <Col md={3} sm={6} xs={12}>
+                  <div
+                    key={index}
+                    className="product-details-container-single-product"
+                  >
+                    <article>
+                      <Link
+                        to="/product-lehenga"
+                        className="product-details-container-single-product"
+                      >
+                        <img
+                          src={product.productimgae}
+                          alt="Image of Maroon Lehenga"
+                        />
+                        <p className="product-details-container-single-product__name">
+                          {product.productName}{" "}
+                        </p>
+                        <p className="product-details-container-single-product__price">
+                          {" "}
+                          {product.productPrice}{" "}
+                        </p>
+                      </Link>
+                    </article>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+          <div className="NewProduct-S-md">
+            <Row>
+              {newproductarray.map((product, index) => (
+                <Col>
+                  <div
+                    key={index}
+                    className="product-details-container-single-product"
+                  >
+                    <article>
+                      <Link
+                        to="/product-lehenga"
+                        className="product-details-container-single-product"
+                      >
+                        <img
+                          src={product.productimgae}
+                          alt="Image of Maroon Lehenga"
+                        />
+                        <p className="product-details-container-single-product__name">
+                          {product.productName}{" "}
+                        </p>
+                        <p className="product-details-container-single-product__price">
+                          {" "}
+                          {product.productPrice}{" "}
+                        </p>
+                      </Link>
+                    </article>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default NewProduct;
