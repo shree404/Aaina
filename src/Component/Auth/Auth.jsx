2b42d03e088b56a3dee882e4a6070dbd0b3bc4c8
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-26 12:28:15 +0545

    Login Page is Added

--- src/Component/Auth/Auth.jsx
+++ src/Component/Auth/Auth.jsx
@@ -1,8 +1,48 @@
-function Auth(){
-    return(
-        <>
-        </>
-    )
+import Login from "./Login";
+import { useState } from "react";
+import Register from "./Register";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Auth() {
+  const [authtype, setAuthtype] = useState("login");
+  return (
+    <>
+      <div className="Auth">
+        <Row className="justify-content-md-center">
+          <Col md={2}>
+            <button
+              onClick={() => setAuthtype("login")}
+              className={`Auth-button ${
+                authtype === "login" ? "auth-button" : ""
+              }`}
+            >
+              Login
+            </button>{" "}
+          </Col>
+          <Col md={2}>
+            <button
+              onClick={() => setAuthtype("register")}
+              className={`Auth-button ${
+                authtype === "register" ? "auth-button" : ""
+              }`}
+            >
+              Register
+            </button>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md={6} sm={12} xs={12}>
+            {authtype === "login" && <Login Signup={() => setAuthtype("register")}
+              className={`Auth-button ${
+                authtype === "register" ? "auth-button" : ""
+              }`} />}
+            {authtype === "register" && <Register />}
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
 
 export default Auth;
---
+++ src/Pages/Authpage.jsx
@@ -0,0 +1,14 @@
+import Auth from "../Component/Auth/Auth";
+import Nav from "../Component/LandingPage/Nav";
+import Footer from "../Component/LandingPage/Footer";
+
+function AuthPage(){
+    return(
+        <>
+        <Nav/>
+        <Auth/>
+        <Footer/>
+        </>
+    )
+}
+export default AuthPage;
