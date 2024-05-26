9c31abbe91e81d7cf692f23f2e23e5531451a258
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-26 14:06:30 +0545

    Login Register Page Added

--- src/Component/Auth/Auth.jsx
+++ src/Component/Auth/Auth.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 import Register from "./Register";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Container from 'react-bootstrap/Container';
 
 function Auth() {
   const [authtype, setAuthtype] = useState("login");
+
   return (
     <>
       <div className="Auth">
+        <Container>
         <Row className="justify-content-md-center">
-          <Col md={2}>
+          <Col md={2} sm={6} xs={6}>
             <button
               onClick={() => setAuthtype("login")}
               className={`Auth-button ${
@@ -20,7 +23,7 @@ function Auth() {
               Login
             </button>{" "}
           </Col>
-          <Col md={2}>
+          <Col md={2}  sm={6} xs={6}>
             <button
               onClick={() => setAuthtype("register")}
               className={`Auth-button ${
@@ -32,14 +35,18 @@ function Auth() {
           </Col>
         </Row>
         <Row className="justify-content-md-center">
-          <Col md={6} sm={12} xs={12}>
+          <Col md={7} sm={12} xs={12}>
             {authtype === "login" && <Login Signup={() => setAuthtype("register")}
               className={`Auth-button ${
                 authtype === "register" ? "auth-button" : ""
               }`} />}
-            {authtype === "register" && <Register />}
+            {authtype === "register" && <Register Login={() => setAuthtype("login")}
+              className={`Auth-button ${
+                authtype === "login" ? "auth-button" : ""
+              }`} />}
           </Col>
         </Row>
+        </Container>
       </div>
     </>
   );
