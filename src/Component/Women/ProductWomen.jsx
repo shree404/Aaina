4889a01d71089140f956a81bdfcbf4981c47a1f3
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-06-23 15:56:09 +0545

    Filter Option is added in kurti list

--- src/Component/Women/ProductWomen.jsx
+++ src/Component/Women/ProductWomen.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import products from "../../products.json"; // Import your products data
 import ProductCard from "../ProductCard";
 
-function WomenProductList({ selectedColors }) {
-  const womenProducts = products.filter((product) => {
-    return product.gendercat === "women" && (selectedColors.length === 0 || selectedColors.includes(product.color));
-  });
+function WomenProductList({ products, selectedColors, sortBy }) {
+  let womenProducts = products.filter(
+    (product) =>
+      product.gendercat === "women" &&
+      (selectedColors.length === 0 || selectedColors.includes(product.color))
+  );
+
+  let sortedProducts = [...womenProducts];
+  switch (sortBy) {
+    case "Price High To Low":
+      sortedProducts.sort((a, b) => parseFloat(b.Price.slice(3)) - parseFloat(a.Price.slice(3)));
+      break;
+    case "Price Low To High":
+      sortedProducts.sort((a, b) => parseFloat(a.Price.slice(3)) - parseFloat(b.Price.slice(3)));
+      break;
+    case "Latest Arrival":
+      sortedProducts.sort(
+        (a, b) => new Date(b.arrival_date) - new Date(a.arrival_date)
+      );
+      break;
+    default:
+      break;
+  }
 
   return (
     <Container>
       <div className="WomenFilterRed">
         <Row>
-          {womenProducts.map((product, index) => (
+          {sortedProducts.map((product, index) => (
             <ProductCard product={product} key={index} md={6} lg={6} xl={4} />
           ))}
         </Row>
